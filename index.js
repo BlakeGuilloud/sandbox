836a23e457be2a05327138d09612ffd90f1e6ee3
Forrest Guilloud <fbguillo@gmail.com>
2017-06-02 21:27:31 -0400

    built function

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+function stripAndShape(number) {
+  let newData = data.replace(/[^0-9]/g, '');
+
+  if (newData[0] === '1') {
+    newData = `+${newData}`;
+  } else {
+    newData = `+1${newData}`;
+  }
+
+  return newData;
+}
+
+module.exports = stripAndShape;
