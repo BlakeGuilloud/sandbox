450bae0e75b1789b62364bb86c869bd73f1f356d
Forrest Guilloud <fbguillo@gmail.com>
2017-06-06 12:31:28 -0400

    stash

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -30,7 +30,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'git@github.com:BlakeGuilloud/sandbox.git',
       path : '/home/ec2-user/sandbox',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
 };
